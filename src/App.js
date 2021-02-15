62ea9cf8e3237bba30603926a8a56de2ed9afc5d
isaac93romano@gmail.com <isaac93romano@gmail.com>
2021-02-15 15:36:56 -0500

    added 3 files to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import Header from './Header';
+import Card from './Card';
+import ContactContainer from './ContactContainer';
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<main>
-					<p>Appraisal Gem Lab</p>
-					<img src='https://imgur.com/HrKKVOu.jpg' alt='logo'/>
-				</main>
+			<div id='body'>
+				<Header />
+				<Card />
+				<ContactContainer />
+				<p>Appraisal Gem Lab</p>
+				<img src='https://imgur.com/HrKKVOu.jpg' alt='logo' />
 			</div>
 		);
 	}
---
+++ src/ContactContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContactContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ContactContainer;
