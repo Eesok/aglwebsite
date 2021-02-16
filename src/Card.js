b4ee58eb98d7481fd56f6d419d7a153824cf43ea
isaac93romano@gmail.com <isaac93romano@gmail.com>
2021-02-15 19:57:00 -0500

    made a dev branch and edited favicon

--- src/Card.js
+++ src/Card.js
@@ -11,7 +11,7 @@ class Card extends Component {
 						sit amet pretium urna. Vivamus venenatis velit nec neque ultricies,
 						eget elementum magna tristique. Quisque vehicula, risus eget aliquam
 						placerat, purus leo tincidunt eros, eget luctus quam orci in velit.
-						Praesent scelerisque tortor sed accumsan convallis.
+						Praesent scelerisque tortor sed accumsan convalli.
 					</p>
 				</div>
 				<div class='column'>
